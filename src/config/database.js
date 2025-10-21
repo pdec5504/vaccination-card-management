f02cd8b86a7b1eefab0b62b47b80227fac37f6ef
Pedro Dec <pdec5504@gmail.com>
2025-10-21 11:44:45 -0300

    feat: modify `database.js` structure to use test database

--- src/config/database.js
+++ src/config/database.js
@@ -1,14 +1,27 @@
 const mongoose = require("mongoose");
 require("dotenv").config();
 
-const MONGO_URI = `mongodb://${process.env.DB_USER}:${process.env.DB_PASS}@${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_NAME}?authSource=admin`;
+const isTestEnvironment = procces.env.NODE_ENV === "test";
+
+const MONGO_URI = isTestEnvironment
+  ? procces.env.MONGO_URI_TEST
+  : `mongodb://${process.env.DB_USER}:${process.env.DB_PASS}@${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_NAME}?authSource=admin`;
+
+if (!MONGO_URI) {
+  console.error(
+    "ERROR: MONGO_URI not defined. Verify .env file or tests configuration."
+  );
+  procces.exit(1);
+}
 
 const connectDB = async () => {
   try {
     await mongoose.connect(MONGO_URI);
-    console.log("Database connected.");
+    if (!isTestEnvironment) {
+      console.log("Database connected.");
+    }
   } catch (error) {
-    console.error("Error connecting to database:", error.massage);
+    console.error("Error connecting to database:", error.message);
     process.exit(1);
   }
 };
