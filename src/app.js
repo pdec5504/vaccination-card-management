c86cf219f5132706a87f872bc0eb714d5dee4f18
Pedro Dec <pdec5504@gmail.com>
2025-10-21 09:41:18 -0300

    feat: add module routes in `app.js`

--- src/app.js
+++ src/app.js
@@ -1,10 +1,24 @@
 const express = require("express");
-
 const app = express();
+
 app.use(express.json());
+
+const vaccineRoutes = require("./modules/vaccines/vaccine.routes");
+const usersRoutes = require("./modules/users/user.routes");
+const vaccinationRoutes = require("./modules/vaccinations/vaccination.routes");
+
+const vaccinationController = require("./modules/vaccinations/vaccination.controller");
 
 app.get("/health", (req, res) => {
   res.status(200).json({ status: "API is working" });
 });
+
+app.use("/api/vaccines", vaccineRoutes);
+
+app.use("/api/users", usersRoutes);
+
+app.use("/api/vaccinations", vaccinationRoutes);
+
+app.get("/api/users/:id/card", vaccinationController.getUserCard);
 
 module.exports = app;
