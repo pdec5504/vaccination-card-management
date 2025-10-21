f341a4a40eee2daefde0d4b49e67fbec2441b08e
Pedro Dec <pdec5504@gmail.com>
2025-10-21 18:10:37 -0300

    feat: add `setup.js` and `teardown.js` files for jest configuration

---
+++ tests/setup.js
@@ -0,0 +1,33 @@
+const mongoose = require("mongoose");
+require("dotenv").config();
+// const connectDB = require("../src/config/database");
+
+module.exports = async () => {
+  process.env.NODE_ENV = "test";
+  console.log('\n[Global Setup] NODE_ENV defined as "test".');
+
+  const dbNameTest = process.env.DB_NAME_TEST;
+
+  if (
+    !process.env.DB_USER ||
+    !process.env.DB_PASS ||
+    !process.env.DB_HOST ||
+    !process.env.DB_PORT ||
+    !dbNameTest
+  ) {
+    console.error(
+      "[Global Setup] ERRO DE CONFIGURAÇÃO: Faltam variáveis de base de dados para teste no .env"
+    );
+    process.exit(1);
+  }
+  const MONGO_URI_TEST = `mongodb://${process.env.DB_USER}:${process.env.DB_PASS}@${process.env.DB_HOST}:${process.env.DB_PORT}/${dbNameTest}?authSource=admin`;
+
+  try {
+    console.log("[Global Setup] Connectiong to test database.");
+    await mongoose.connect(MONGO_URI_TEST);
+    console.log("[Global Setup] Test database succesfully connected.");
+  } catch (error) {
+    console.error("[Global Setup] Error connecting to test database:", error);
+    process.exit(1);
+  }
+};
